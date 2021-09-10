6b4814447938c17f12a156d2cfa0a5e0f9883d69
liamizaguirre <liamizaguirre@gmail.com>
2021-09-10 11:40:22 -0300

    añadiendo trabajo

---
+++ src/Container/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Main from "../Components/Historia";
+
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className ="App">
+        <Main />
+      </div>
+    )
+  }
+}
+
+export default App;
